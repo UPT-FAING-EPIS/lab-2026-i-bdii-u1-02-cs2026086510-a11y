32135dc6cc5e3d68189cd99f0504c80b4925e12c
Cristian Soto <cs2026086510@virtual.upt.pe>
2026-05-02 20:53:54 -0400

    Submit Laboratorio

---
+++ ServicioCliente/ClienteAPI/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClienteAPI.Models;
+
+public partial class Cliente
+{
+    public int IdCliente { get; set; }
+
+    public string NomCliente { get; set; } = null!;
+
+    public virtual ICollection<ClientesDocumento> ClientesDocumentos { get; } = new List<ClientesDocumento>();
+}
---
+++ ServicioCliente/ClienteAPI/Models/TiposDocumento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClienteAPI.Models;
+
+public partial class TiposDocumento
+{
+    public byte IdTipoDocumento { get; set; }
+
+    public string DesTipoDocumento { get; set; } = null!;
+
+    public virtual ICollection<ClientesDocumento> ClientesDocumentos { get; } = new List<ClientesDocumento>();
+}
